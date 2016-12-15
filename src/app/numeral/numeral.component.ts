2a9aa59bb9be178372e5b4067585766a477e1619
Koll Johnson <koll-johnson@openmailbox.org>
2016-12-15 14:05:27 -0600

    works for all tests cases of converting arabic to roman

--- src/app/numeral/numeral.component.ts
+++ src/app/numeral/numeral.component.ts
@@ -13,7 +13,8 @@ export class NumeralComponent {
 
     convert():void {
         this.result= "";
-        let roman = {V: 5, IV: 4, I: 1};
+        // lookup table of roman numerals and respective values
+        let roman = {M: 1000, CM: 900, D: 500, C: 100, L: 50, LX: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
         let currentNumber: number = parseInt(this.numeral);
         for(let i in roman) {
             while(currentNumber >= roman[i]) {
