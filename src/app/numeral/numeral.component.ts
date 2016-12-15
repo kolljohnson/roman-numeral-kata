dd66e2c43a53aa2f3fefa3a3adbf7b4440b1b119
Koll Johnson <koll-johnson@openmailbox.org>
2016-12-15 13:59:33 -0600

    entering 1 returns I

--- src/app/numeral/numeral.component.ts
+++ src/app/numeral/numeral.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'numeral',
-    template: '<h3>Numeral Converter</h3>'
+    templateUrl: 'numeral.component.html'
 })
 
 export class NumeralComponent {
+    public numeral: string = "";
+    public result: string = "";
 
+
+    convert():void {
+        let roman = {V: 5, IV: 4, I: 1};
+        let currentNumber: number = parseInt(this.numeral);
+        for(let i in roman) {
+            while(currentNumber === roman[i]) {
+                this.result += i;
+                currentNumber -= roman[i];
+            }
+        }
+    }
 }
